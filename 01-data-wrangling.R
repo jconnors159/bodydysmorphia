aaa1b98bd6d35c302aaf3e49166375108468c4d4
Nizan <nmh1@arizona.edu>
2022-03-28 22:07:45 -0400

    # switched portion to the wrangling file

--- 01-data-wrangling.R
+++ 01-data-wrangling.R
@@ -14,4 +14,14 @@ survey_data_clean %>%
   count(DistributionChannel)
 
 # save clean file to disk
-write_csv(survey_data_clean, "processed-data/survey_data_clean.csv")
+write_csv(survey_data_clean, "survey_data_clean.csv")
+
+
+bdd_survey_data <- read_csv("survey_data_clean.csv")
+
+# Nate's portion of cleaning:
+bdd_survey_data <- bdd_survey_data[-c(1,2),]
+bdd_survey_data <- filter(bdd_survey_data, Q1 != "I do not accept to participate in this research project")
+bdd_survey_data <- read_csv("survey_data_clean.csv")
+
+
