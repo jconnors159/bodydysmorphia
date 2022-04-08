8e5f522115c11e1da47f624162b28eb4087b56ce
Hayli Green <haylig@email.arizona.edu>
2022-04-07 19:27:31 -0700

    Added kNN code. It definitely has errors so feel free to comment it out if needed. Going to ask Adriana about the error(s) tomorrow.

--- 01-data-wrangling.R
+++ 01-data-wrangling.R
@@ -2,7 +2,8 @@
 library(tidyverse)
 library(stringr)
 library(dplyr)
-
+library(ISLR)
+library(caret)
 # read data in
 survey_data <- read_csv("social_media_033022.csv")
 
@@ -43,10 +44,35 @@ cp_bdd_survey_data %>% count(Q23_final, Q23_modified) %>% view()
 cp_bdd_survey_data <- select(bdd_survey_data, -c(Status, UserLanguage, DistributionChannel))
 
 
+#Hayli kNN code:
 
+#Preprocessing data w/ one-hot encoding
+bdd_k <- bdd_survey_data
+bdd_dummy <- dummyVars(BDD_Score ~ ., data = bdd_k, fullRank = TRUE)
 
+bdd_k <- predict(bdd_dummy, newdata = bdd_k)
+bdd_k <- data.frame(bdd_k)
 
+#Re-adding target variable (BDD_Score) to dataset bc dummyVars dropped it
+score_vals <- cp_bdd_survey_data %>%
+  select(BDD_Score)
+bdd_k <- cbind(bdd_k, score_vals)
 
+#Training data
+bdd_split <- createDataPartition(bdd_k$BDD_Score, p = 0.8, list = FALSE)
+features_train <- bdd_k[bdd_split, !(names(bdd_k) %in% c('BDD_Score'))]
+features_test <- bdd_k[-bdd_split, !(names(bdd_k) %in% c('BDD_Score'))]
+target_train <- bdd_k[bdd_split, "BDD_Score"]
+target_test <- bdd_k[-bdd_split, "BDD_Score"]
+preprocess_object <- preProcess(features_train, 
+                                method = c('center', 'scale', 'knnImpute'))
+
+features_train <- predict(preprocess_object, newdata = features_train)
+features_test <- predict(preprocess_object, newdata = features_test)
+
+#Fitting kNN model 
+knn_fit <- knn3(features_train, target_train, k = 5)
+knn_pred <- predict(knn_fit, features_test, type = 'class' )
 
 
 
