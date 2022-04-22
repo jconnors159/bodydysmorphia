d860aa59f80d8a269227dfdb518ea4266e82cbfc
Jocelyn Connors <jocelynconnors@email.arizona.edu>
2022-04-21 23:26:09 -0700

    Added paragraphs

--- final_dashboard.R
+++ final_dashboard.R
@@ -156,7 +156,6 @@ header <- dashboardHeader(title = "Cross Study of Body Dismorphia and Social Med
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Dashboard", tabName = "Dashboard", icon = icon("tachometer-alt")),
-    menuItem("Widgets", tabName = "Widgets", icon = icon("th")),
     menuItem("GitHub", href = "https://github.com/jconnors159/bodydysmorphia", icon = icon("code-branch")),
     
     
@@ -176,86 +175,112 @@ body <-   dashboardBody(
   
   tabItems(
     # page 1 ----
-    tabItem(tabName = "Dashboard", "Dashboard content.",
+    tabItem(tabName = "Dashboard",
                                 fluidRow(
                                 box(width='50%',
-                                h3("BDD Scores across Genders"),column(width = 12, plotlyOutput("plot1"),
-                                HTML("<p>For the Shiny dashboard we will insert graphs into the statistical analysis portions:</p>
-                                      <ul><li> Line of code where the graph is</li>
-                                      <li>Summary and results of findings (will be placed underneath the graph) (paragraph) 
-                                      Title of graph plot <> X, Y  label names you would like to include 
-                                      Legend title (what your filter may be by </li></ul>
+                                h3("BDD Scores Across Genders"),column(width = 12, plotlyOutput("plot1"),
+                                HTML("<br>"),
+                                HTML("<p>“BDD Scores across Genders” is a histogram that looks at the distribution of BBD Scores by 
+                                personal pronouns (he, she, or they). The graph shows more of a binomial distribution of spread with 
+                                one peak value demonstrated on the “she” pronouns with a score of 30.00. A peak from male-identified 
+                                pronouns had a score of 21.3. Lastly, the “they” pronouns with a score of 22.6.</p>
 ")))), # string name must match with sever 
                                 HTML("<br>"), 
                                 fluidRow(
                                 box(width='50%', 
-                                h3("Class Standing across Social Media Platforms "), plotlyOutput("plot2"))),
+                                h3("Class Standing Across Social Media Platforms "), plotlyOutput("plot2"),
+                                HTML("<br>"),
+                                HTML("<p>“Class Standing across Social Media Platforms” identifies what each class standing utilizes, 
+                                     as well as what platform, and opens the question of interest on what social media platforms should 
+                                     be used in further analysis. Overall, the graph shows that Instagram is the most used application 
+                                     among Freshmen, Sophomores, and Juniors, with Seniors being the only exception of having a higher 
+                                     count on YouTube.<p>"))),
                                 fluidRow(
                                 box(width='100%',
-                                h3("Topics explored on Social Media"), plotlyOutput("plot3"))
+                                h3("Topics Explored on Social Media"), plotlyOutput("plot3"),
+                                HTML("<br>"),
+                                HTML("<p>“Topics Explored on Social Media” showcases the BDD scores' distribution across different 
+                                     entertainment sources explored on social media. This is indexed by the Age group. There is a 
+                                     higher shift of scores in the Technology/Smartphone section in the age group range of 18-20  
+                                     versus the 25+ age range showing a higher distribution in the topic of Celebrities.  <p>"))
               )),
-    
-    # page 2 ----
-    tabItem(tabName = "Widgets", "Widgets content.",
-            fluidRow(
-              #box(plotOutput("plot1", height = 250))
-            )),
+
     
     
     # page 3 ----
     tabItem(tabName = "linear_regression",
-              h1("Linear Regression content."),
-              fluidRow(h3("Average BDD score based on hours spent on social media daily"),
-              box(width='100%',plotOutput("multiplot")),
+              fluidRow(h3("Average BDD Score Based on Hours Spent on Social Media Daily"),
+              box(width='100%',plotOutput("multiplot"),
+                  HTML("<br>"),
+                  HTML("<p> This graph shows the average BDD score of the participants of the survey based on how much time 
+                  they spend on social media daily. There were three given options on the question, less than 3 hours/day, 
+                  3-10 hours/day and 10-20 hours/day. The only significant value of the three was 10-20 hours/day with a p-value 
+                  of 0.0437 and had an average BDD Score of 10 points higher than both of the other options . We also found that 
+                  there was only a .1 difference in average BDD scores of those who spent 3 - 10 hours a day on social media versus 
+                  those who spend less than 3 hours a day.  The r-squared value for the model comes out to 0.03 meaning that time 
+                  spent on social media explains approximately 3% of the variance in BDD scores. While this is helpful to understand 
+                  how time spent on social media can affect BDD scores, since there is a low level of confidence therefore we are unable 
+                  to say that time spent on social media daily has an effect on BDD score.
+<p>")),
               )),
     # page 4 ----
     tabItem(tabName = "logistic_regression",
-            h1("Logistic Regression content."),
             fluidRow(
               fluidPage(
-                box(width='100%',h3(" “Average BDD Score Based on Age Range”"),plotOutput("log_boxplot")), 
-                HTML("<p>The purpose of the first graph is to show the average BDD Score based on each of the three age 
-                ranges and if someone in the younger adult age (18-20) range would have a higher score than someone in one 
-                of the two other ranges (21-24 or 25+). The only significant p-value among all variables was 18-20, which had 
-                a p-value of 0.01. This means that there is some correlation between this age group and BDD Scores, but more 
-                correlation compared to the age groupings. The p-value of 21-24 was 0.73 and 25+ was 0.69. There is insufficient 
-                evidence to conclude that a relationship exists between the two older age groupings and their effect on the BDD Score. 
-                <p>"),
+                box(width='100%',h3(" “Average BDD Score Based on Age Range”"),plotOutput("log_boxplot"),
+                    HTML("<br>"),
+                    HTML("<p>The purpose of the first graph is to show the average BDD Score based on each of the three age 
+                        ranges and if someone in the younger adult age (18-20) range would have a higher score than someone in one 
+                        of the two other ranges (21-24 or 25+). The only significant p-value among all variables was 18-20, which had 
+                        a p-value of 0.01. This means that there is some correlation between this age group and BDD Scores, but more 
+                        correlation compared to the age groupings. The p-value of 21-24 was 0.73 and 25+ was 0.69. There is insufficient 
+                        evidence to conclude that a relationship exists between the two older age groupings and their effect on the BDD Score. 
+                        <p>")),
+
                 HTML("<br>"),
-                box(width='100%',h3("“Probability of Age Influencing BDD Score”"), plotOutput("log_sumplot")),
-                HTML("<p>This is further proven by looking at the second graph (Probability of Age Influencing BDD Score) 
-                and viewing how each age range has a probability of having an effect on the BDD Score. The effect, or fit, 
-                in this instance is the predicted values given by the logistic regression model. What this means for the plot 
-                above, is that the predicted values (0.34, 0.31, and 0.39) are given based on the outcome, the outcome being 
-                the independent variables (18-20, 21-24, and 25+). The predicted values show the probability of there being an 
-                effect of age on BDD Scores is fairly low for each variable. They all equal about 0.35 on average. 
-                <p>")
+                box(width='100%',h3("“Probability of Age Influencing BDD Score”"), plotOutput("log_sumplot"),
+                    HTML("<br>"),
+                    HTML("<p>This is further proven by looking at the second graph (Probability of Age Influencing BDD Score) 
+                        and viewing how each age range has a probability of having an effect on the BDD Score. The effect, or fit, 
+                        in this instance is the predicted values given by the logistic regression model. What this means for the plot 
+                        above, is that the predicted values (0.34, 0.31, and 0.39) are given based on the outcome, the outcome being 
+                        the independent variables (18-20, 21-24, and 25+). The predicted values show the probability of there being an 
+                        effect of age on BDD Scores is fairly low for each variable. They all equal about 0.35 on average. 
+                        <p>")),
+
               ))),
     
     # page 5 ----
     tabItem(tabName = "cluster_analysis",
-            h1("Cluster Analysis content."),
-            fluidRow(box(width='100%', h3("Cluster Analysis of Average BDD Scores"),plotOutput("clusterplot"))
+            fluidRow(box(width='100%', h3("Cluster Analysis of Average BDD Scores"),plotOutput("clusterplot"), 
+                         HTML("<br>"),
+                         HTML("<p>This model breaks the data into two clusters, or groups, where the first cluster consists of 
+                         74 people who took the survey and the second cluster consists of 62 people. This plot shows that the people 
+                         in cluster 1 have, on average, a higher BDD score than those in cluster 2. These are individuals who not only 
+                         experience appearance-related harassment online and frequently compare themselves to famous people on social 
+                         media, but people who also spend, on average, over 3 hours a day on these social networking sites.
+<p>"))
               )),
     # page 6 ----
     tabItem(tabName = "knn_analysis", 
-            h1("kNN Analysis content."),
             fluidRow(box(width='20%',h3("KNN Analysis with Social Media Platforms"), plotOutput("kkn1"),
-                               HTML("<p> With all social media platform options (TikTok, YouTube, Instagram, Facebook, 
-                                   Snapchat, Pinterest) selected as features, KNN analysis shows that the platform an individual 
-                                   uses doesn’t have a significant impact on BDD score. While the algorithm correctly predicted 11 
-                                   high scores as high, 8 low scores were incorrectly classified as high. 5 high scores were incorrectly 
-                                   predicted to be low. Only 3 low scores were predicted correctly by the KNN algorithm. This shows that 
-                                   the algorithm overdiagnosed many as having a high BDD score when they in fact did not. <p>")),
+                         HTML("<br>"),
+                         HTML("<p> With all social media platform options (TikTok, YouTube, Instagram, Facebook, 
+                             Snapchat, Pinterest) selected as features, KNN analysis shows that the platform an individual 
+                             uses doesn’t have a significant impact on BDD score. While the algorithm correctly predicted 11 
+                             high scores as high, 8 low scores were incorrectly classified as high. 5 high scores were incorrectly 
+                             predicted to be low. Only 3 low scores were predicted correctly by the KNN algorithm. This shows that 
+                             the algorithm overdiagnosed many as having a high BDD score when they in fact did not. <p>")),
                      HTML("<br>"),
             fluidRow(box(width='20%',h3("KNN Analysis with BDD Questionnaire Questions"), plotOutput("kkn2"),
-                              HTML("<p> With all of the BDD-score-determining questions selected as features, KNN much 
-                                  more accurately predicts the scores of participants. This, of course, is to be expected. 
-                                  15 actual high scores are correctly predicted to be high while only one was incorrectly 
-                                  predicted to be low. Meanwhile, 8 low scores were correctly predicted as low while 3 were 
-                                  incorrectly predicted as high. While there is still a trend of overdiagnosing low scores as 
-                                  high, we see a more accurate analysis made by the KNN algorithm when taking into account BDD 
-                                  Test questions.</p>
+                             HTML("<br>"),
+                             HTML("<p> With all of the BDD-score-determining questions selected as features, KNN much 
+                                 more accurately predicts the scores of participants. This, of course, is to be expected. 
+                                 15 actual high scores are correctly predicted to be high while only one was incorrectly 
+                                 predicted to be low. Meanwhile, 8 low scores were correctly predicted as low while 3 were 
+                                 incorrectly predicted as high. While there is still a trend of overdiagnosing low scores as 
+                                 high, we see a more accurate analysis made by the KNN algorithm when taking into account BDD 
+                                 Test questions.</p>
                                       
 ")))))
     
