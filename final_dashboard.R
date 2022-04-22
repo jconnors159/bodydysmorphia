f050ef88b73cd783c3eb45708b6f8599f34790b3
Nizan <nmh1@arizona.edu>
2022-04-22 13:40:05 -0400

    "size dimensions have been added"

--- final_dashboard.R
+++ final_dashboard.R
@@ -122,7 +122,7 @@ set.seed(888)
 cp_bdd_survey_data <- cp_bdd_survey_data %>%
   mutate(BDD_Categories = case_when(BDD_Score < 20 ~ "low",
                                     BDD_Score >= 20 ~ "high"))
-print(colnames(cp_bdd_survey_data))
+
 bdd_k <- cp_bdd_survey_data
 
 bdd_dummy <- dummyVars(BDD_Categories ~  tiktok + youtube + instagram + facebook + snapchat + pinterest, data = bdd_k, fullRank = TRUE)
@@ -175,10 +175,10 @@ body <-   dashboardBody(
   
   tabItems(
     # page 1 ----
-    tabItem(tabName = "Dashboard",
-                                fluidRow(
-                                box(width='50%',
-                                h3("BDD Scores Across Genders"),column(width = 12, plotlyOutput("plot1"),
+    tabItem(tabName = "Dashboard",fluidPage(
+                                fluidRow( column(12,
+                                box(width = '12',
+                                h3("BDD Scores Across Genders"), plotlyOutput("plot1"),
                                 HTML("<br>"),
                                 HTML("<br>"),
                                 HTML("<p>“BDD Scores across Genders” is a histogram that looks at the distribution of BBD Scores by 
@@ -187,8 +187,8 @@ body <-   dashboardBody(
                                 pronouns had a score of 21.3. Lastly, the “they” pronouns with a score of 22.6.</p>
 ")))), # string name must match with sever 
                                 HTML("<br>"), 
-                                fluidRow(
-                                box(width='50%', 
+                                fluidRow(column(12, 
+                                box(width = '12', 
                                 h3("Class Standing Across Social Media Platforms "), plotlyOutput("plot2"),
                                 HTML("<br>"),
                                 HTML("<br>"),
@@ -196,24 +196,26 @@ body <-   dashboardBody(
                                      as well as what platform, and opens the question of interest on what social media platforms should 
                                      be used in further analysis. Overall, the graph shows that Instagram is the most used application 
                                      among Freshmen, Sophomores, and Juniors, with Seniors being the only exception of having a higher 
-                                     count on YouTube.<p>"))),
-                                fluidRow(
-                                box(width='100%',
+                                     count on YouTube.<p>")))),
+                                fluidRow(column(12,
+                                box(width = '12',
                                 h3("Topics Explored on Social Media"), plotlyOutput("plot3"),
                                 HTML("<br>"),
                                 HTML("<br>"),
                                 HTML("<p>“Topics Explored on Social Media” showcases the BDD scores' distribution across different 
                                      entertainment sources explored on social media. This is indexed by the Age group. There is a 
                                      higher shift of scores in the Technology/Smartphone section in the age group range of 18-20  
-                                     versus the 25+ age range showing a higher distribution in the topic of Celebrities.  <p>"))
-              )),
+                                     versus the 25+ age range showing a higher distribution in the topic of Celebrities.  <p>"),
+                                HTML("<br>"))
+              )))),
 
     
     
     # page 3 ----
     tabItem(tabName = "linear_regression",
-              fluidRow(h3("Average BDD Score Based on Hours Spent on Social Media Daily"),
-              box(width='100%',plotOutput("multiplot"),
+              fluidRow(column(12,
+              box( width = "12",
+                  h3("Average BDD Score Based on Hours Spent on Social Media Daily"), plotOutput("multiplot"),
                   HTML("<br>"),
                   HTML("<br>"),
                   HTML("<p> This graph shows the average BDD score of the participants of the survey based on how much time 
@@ -225,13 +227,13 @@ body <-   dashboardBody(
                   spent on social media explains approximately 3% of the variance in BDD scores. While this is helpful to understand 
                   how time spent on social media can affect BDD scores, since there is a low level of confidence therefore we are unable 
                   to say that time spent on social media daily has an effect on BDD score.
-<p>")),
+          <p>"))),
               )),
     # page 4 ----
     tabItem(tabName = "logistic_regression",
-            fluidRow(
-              fluidPage(
-                box(width='100%',h3(" “Average BDD Score Based on Age Range”"),plotOutput("log_boxplot"),
+            fluidPage(
+              fluidRow( column(12, 
+                box(width='12',h3(" “Average BDD Score Based on Age Range”"),plotOutput("log_boxplot"),
                     HTML("<br>"),
                     HTML("<br>"),
                     HTML("<p>The purpose of the first graph is to show the average BDD Score based on each of the three age 
@@ -240,11 +242,12 @@ body <-   dashboardBody(
                         a p-value of 0.01. This means that there is some correlation between this age group and BDD Scores, but more 
                         correlation compared to the age groupings. The p-value of 21-24 was 0.73 and 25+ was 0.69. There is insufficient 
                         evidence to conclude that a relationship exists between the two older age groupings and their effect on the BDD Score. 
-                        <p>")),
+                        <p>")))),
 
                 HTML("<br>"),
                 HTML("<br>"),
-                box(width='100%',h3("“Probability of Age Influencing BDD Score”"), plotOutput("log_sumplot"),
+                fluidRow( column(12, 
+                  box(width='12',h3("“Probability of Age Influencing BDD Score”"), plotOutput("log_sumplot"),
                     HTML("<br>"),
                     HTML("<p>This is further proven by looking at the second graph (Probability of Age Influencing BDD Score) 
                         and viewing how each age range has a probability of having an effect on the BDD Score. The effect, or fit, 
@@ -252,13 +255,14 @@ body <-   dashboardBody(
                         above, is that the predicted values (0.34, 0.31, and 0.39) are given based on the outcome, the outcome being 
                         the independent variables (18-20, 21-24, and 25+). The predicted values show the probability of there being an 
                         effect of age on BDD Scores is fairly low for each variable. They all equal about 0.35 on average. 
-                        <p>")),
+                        <p>"))),
 
               ))),
     
     # page 5 ----
     tabItem(tabName = "cluster_analysis",
-            fluidRow(box(width='100%', h3("Cluster Analysis of Average BDD Scores"),plotOutput("clusterplot"), 
+            fluidRow( column(12,
+              box(width='12', h3("Cluster Analysis of Average BDD Scores"),plotOutput("clusterplot"), 
                          HTML("<br>"),
                          HTML("<br>"),
                          HTML("<p>This model breaks the data into two clusters, or groups, where the first cluster consists of 
@@ -266,11 +270,13 @@ body <-   dashboardBody(
                          in cluster 1 have, on average, a higher BDD score than those in cluster 2. These are individuals who not only 
                          experience appearance-related harassment online and frequently compare themselves to famous people on social 
                          media, but people who also spend, on average, over 3 hours a day on these social networking sites.
-<p>"))
+                        <p>")))
               )),
     # page 6 ----
     tabItem(tabName = "knn_analysis", 
-            fluidRow(box(width='20%',h3("KNN Analysis with Social Media Platforms"), plotOutput("kkn1"),
+            fluidPage(
+              fluidRow( column(12,
+                         box( width = "12",h3("KNN Analysis with Social Media Platforms"), plotOutput("kkn1"),
                          HTML("<br>"),
                          HTML("<br>"),
                          HTML("<p> With all social media platform options (TikTok, YouTube, Instagram, Facebook, 
@@ -278,9 +284,10 @@ body <-   dashboardBody(
                              uses doesn’t have a significant impact on BDD score. While the algorithm correctly predicted 11 
                              high scores as high, 8 low scores were incorrectly classified as high. 5 high scores were incorrectly 
                              predicted to be low. Only 3 low scores were predicted correctly by the KNN algorithm. This shows that 
-                             the algorithm overdiagnosed many as having a high BDD score when they in fact did not. <p>")),
+                             the algorithm overdiagnosed many as having a high BDD score when they in fact did not. <p>")))),
                      HTML("<br>"),
-            fluidRow(box(width='20%',h3("KNN Analysis with BDD Questionnaire Questions"), plotOutput("kkn2"),
+            fluidRow( column(12,
+                      box(width = "12", h3("KNN Analysis with BDD Questionnaire Questions"), plotOutput("kkn2"),
                              HTML("<br>"),
                              HTML("<br>"),
                              HTML("<p> With all of the BDD-score-determining questions selected as features, KNN much 
@@ -289,9 +296,8 @@ body <-   dashboardBody(
                                  predicted to be low. Meanwhile, 8 low scores were correctly predicted as low while 3 were 
                                  incorrectly predicted as high. While there is still a trend of overdiagnosing low scores as 
                                  high, we see a more accurate analysis made by the KNN algorithm when taking into account BDD 
-                                 Test questions.</p>
-                                      
-")))))
+                                 Test questions.</p>")
+                          )))))
     
   )
 )
